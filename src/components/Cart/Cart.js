ec2daca5cfa543d60c0f016bc2d055ec5ef58c3b
Abu-Sahad <abusahad603@gmail.com>
2022-03-26 09:16:24 +0600

    item added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,15 +2,14 @@ import React from 'react';
 import './Cart.css'
 const Cart = (props) => {
     const { cart } = props
-    let name
-    for (const product of cart) {
-        name = product.name
-    }
-
     return (
         <div className='cart'>
             <h4>Selected Clothes:</h4>
-            <p>Name:{name}</p>
+            <div>
+                {cart.map((product) => (
+                    <p key={product.id}>{product.name}</p>
+                ))}
+            </div>
             <span><button>CHOOSE 1 FOR ME</button><br />
                 <button>CHOOSE AGAIN</button></span>
 
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Product.css'
+import { MdAddShoppingCart } from "react-icons/md"
 
 const Product = (props) => {
     const { handleAddToCart, product } = props;
@@ -11,9 +12,10 @@ const Product = (props) => {
                 <p className='product-name'>{name}</p>
                 <p>Price:${price}</p>
             </div>
-            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
-                <p className='btn-text'>Add to Cart</p>
-            </button>
+            <div className='btn-cart-main'>
+                <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+                    <p className='btn-text'>Add to Cart<span className='icons-item'><MdAddShoppingCart /></span></p>
+                </button></div>
         </div>
     );
 };
